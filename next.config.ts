2d32276085f4ce56d489af4b1d88671ad3948b5c
Cayzhed <52002387+CailBzd@users.noreply.github.com>
2025-01-31 11:14:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone', // Permet un bon fonctionnement sur Vercel
   trailingSlash: true,  // Ajoute un "/" à la fin des URLs (utile si besoin d’un chemin absolu)
-  experimental: {
-    appDir: true, // Si tu utilises le dossier 'app'
-  },
 };
 
 export default nextConfig;
