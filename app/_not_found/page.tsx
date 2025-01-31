acd355084d688ba94f2b0f84d08b5d629bb2e1c7
Cayzhed <52002387+CailBzd@users.noreply.github.com>
2025-01-31 10:43:44 +0100

    Update page.tsx

--- app/_not_found/page.tsx
+++ app/_not_found/page.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div>
       <h1>Oups ! Page non trouvée</h1>
-      <p>La page que vous cherchez n'existe pas.</p>
+      <p>La page que vous cherchez n&apos;existe pas.</p>
     </div>
   );
 }
