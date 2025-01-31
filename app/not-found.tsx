a1b2d8bcfe1bfad5c0fc46871dc51d2533b63f3c
Pierre BIZARD <p.bizard@elasoftware.fr>
2025-01-31 14:25:17 +0100

    utilisation de next/link pour les redirection

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-4xl font-bold text-red-600">404 - Page introuvable</h1>
       <p className="mt-2 text-lg text-gray-600">Oups ! La page que vous cherchez n&apos;existe pas.</p>
-      <a href="/" className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+      <Link href="/" className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
         Retour à l&apos;accueil
-      </a>
+      </Link>
     </div>
   );
 }
